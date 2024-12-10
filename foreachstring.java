d26908293fc2044ca5f8ed59e57b835c71cbfff5
hema11920 <kasireddygarihemasree@gmail.com>
2024-12-10 11:12:48 +0530

    Add files via upload

---
+++ foreachstring.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class foreachstring{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a[]={"apple","banana","orange"};
+        for(String b:a)
+        System.out.print(b+",");
+    }
+}
